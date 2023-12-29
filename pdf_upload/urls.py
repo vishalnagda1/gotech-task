ebd5fa4e5cd2c74c0a9404b8d5604853f24006fb
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-29 18:39:47 +0530

    register the list_file api to the apps url

--- pdf_upload/urls.py
+++ pdf_upload/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup, signin, signout, upload_file
+from .views import signup, signin, signout, upload_file, list_files
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('signin/', signin, name='signin'),
     path('signout/', signout, name='signout'),
     path('upload/', upload_file, name='upload_file'),
+    path('list/', list_files, name='list_files'),
 ]
