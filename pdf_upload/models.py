ce9adb206309f93dc198de937c36a2f5109f5b52
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-29 17:01:46 +0530

    create UploadFile model in pdf_upload app

--- pdf_upload/models.py
+++ pdf_upload/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     # add custom fields if needed
     pass
+
+
+class UploadedFile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
