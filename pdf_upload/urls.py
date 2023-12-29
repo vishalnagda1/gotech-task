6b50ceef18749612e0be576cc3c5abbf24926feb
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-29 18:26:54 +0530

    register the api to the apps url config

--- pdf_upload/urls.py
+++ pdf_upload/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import signup, signin, signout
+from .views import signup, signin, signout, upload_file
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('signin/', signin, name='signin'),
     path('signout/', signout, name='signout'),
+    path('upload/', upload_file, name='upload_file'),
 ]
