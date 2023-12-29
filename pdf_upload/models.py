0933b91479ab867561db156a8144a0e599728e35
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-30 00:55:16 +0530

    create a model store extracted images

--- pdf_upload/models.py
+++ pdf_upload/models.py
@@ -10,3 +10,9 @@ class UploadedFile(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+class ExtractedData(models.Model):
+    uploaded_file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE)
+    text = models.TextField()
+    image_paths = models.JSONField()  # Store image paths as a JSON array
