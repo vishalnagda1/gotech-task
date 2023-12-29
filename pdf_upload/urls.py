976f9d02b51168d7558d06328e6bd52e49ec1aa3
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-29 22:54:45 +0530

    register file download api to pdf_upload apps url config

--- pdf_upload/urls.py
+++ pdf_upload/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, signin, signout, upload_file, list_files, rename_file, delete_file
+from .views import signup, signin, signout, upload_file, list_files, rename_file, delete_file, download_file
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('list/', list_files, name='list_files'),
     path('rename/<int:file_id>/', rename_file, name='rename_file'),
     path('delete/<int:file_id>/', delete_file, name='delete_file'),
+    path('download/<int:file_id>/', download_file, name='download_file'),
 ]
