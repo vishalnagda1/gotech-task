9ba27c2f505e42e580d3aa4331e8f71c6b6378b5
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-29 16:35:23 +0530

    register signout api routes in apps url config

--- pdf_upload/urls.py
+++ pdf_upload/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signup, signin
+from .views import signup, signin, signout
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('signin/', signin, name='signin'),
+    path('signout/', signout, name='signout'),
 ]
