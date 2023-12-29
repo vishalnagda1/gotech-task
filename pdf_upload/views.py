d4943208ed5bc22de3698fbbe791665081118928
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-29 15:58:27 +0530

    create signup view in pdf_upload app

--- pdf_upload/views.py
+++ pdf_upload/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+from .models import CustomUser
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+
+        user = CustomUser.objects.create_user(username=username, password=password)
+        return JsonResponse({'message': 'User created successfully'})
